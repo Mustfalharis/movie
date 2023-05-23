a84cc0bfb36f12e40399b47b62cf6c8950700213
Mustfalharis <111998046+Mustfalharis@users.noreply.github.com>
2023-05-23 13:33:20 +0100

    first commit

--- lib/view/screen/home_screen.dart
+++ lib/view/screen/home_screen.dart
@@ -3,10 +3,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../../controller/home_screen_controller.dart';
 import '../widget/home_screen/custom_bottom_appBar_home_screen.dart';
-
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
      Get.put(HomeScreenControllerImp());
